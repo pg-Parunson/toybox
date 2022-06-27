86f78a23cedb41a12cdf2c47f339768a4b366481
Jaeho Jung <iwogh3176@gmail.com>
2022-06-27 16:46:53 +0900

    [init] 프로젝트 초기 환경 세팅

---
+++ src/test/java/com/parunson/toybox/ToyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parunson.toybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
